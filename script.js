a70f9707de9e330d1e694ab40d7b6764b3ac6140
Ryan <dhantranada70@gmail.com>
2025-09-23 13:22:34 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,61 @@
+// 
+    const fromCurrency = document.getElementById("fromCurrency");
+    const toCurrency = document.getElementById("toCurrency");
+    const amountInput = document.getElementById("amount");
+    const result = document.getElementById("result");
+    const convertBtn = document.getElementById("convertBtn");
+
+    // Use your API key from ExchangeRate API(https://app.exchangerate-api.com/dashboard)
+    //Your API Key: 268281b25b9e3259adec19da
+    const apiKey = "268281b25b9e3259adec19da";
+
+    // Load info thru the link or your API key
+    async function loadCurrencies() {
+      const res = await fetch(`https://v6.exchangerate-api.com/v6/${apiKey}/codes`);
+      const data = await res.json();
+      const codes = data.supported_codes;
+
+      // Options for the dropdown of kind of currency
+      codes.forEach(([code, name]) => {
+        const option1 = document.createElement("option");
+        const option2 = document.createElement("option");
+        option1.value = option2.value = code;
+        option1.text = option2.text = `${code} - ${name}`;
+        fromCurrency.appendChild(option1);
+        toCurrency.appendChild(option2);
+      });
+
+
+      fromCurrency.value = "PHP"; // Default values when you open the site
+      toCurrency.value = "USD";   // Default values when you open the site
+    }
+
+    // Convert currency function
+    async function convertCurrency() {
+      const amount = parseFloat(amountInput.value);
+      // isNan() function determines whether a value is NaN (Not-a-Number) or not.
+      if (isNaN(amount) || amount <= 0) {
+        result.textContent = "Please enter a valid amount.";
+        return;
+      }
+      const from = fromCurrency.value;
+      const to = toCurrency.value;
+
+      try {
+        // Fetch requested currency from the the API
+        const res = await fetch(`https://v6.exchangerate-api.com/v6/${apiKey}/pair/${from}/${to}/${amount}`);
+        const data = await res.json();
+
+        if (data.result === "success") {
+          result.textContent = `${amount} ${from} = ${data.conversion_result.toFixed(2)} ${to}`;
+        } else {
+          result.textContent = "Conversion failed";
+        }
+      } catch (err) {
+        result.textContent = "Failed to fetch exchange rate.";
+        console.error(err);
+      }
+    }
+// 
+    convertBtn.addEventListener("click", convertCurrency);// This listens for a click on the "Convert" button.
+    loadCurrencies();// It calls the function that fetches all available currencies (USD, PHP, EUR, etc.) from the API and fills the dropdowns.
